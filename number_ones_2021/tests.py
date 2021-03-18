c0a476cc59b68b85877bfb033502e3a99fdbd5bf
Sean <seanh@zillowgroup.com>
2021-03-17 21:55:41 -0700

    finished

--- number_ones_2021/tests.py
+++ number_ones_2021/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Hits
 
-# Create your tests here.
+class HitsTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        testuser1 = get_user_model().objects.create_user(username='testuser1', password='pass')
+        testuser1.save()
+
+        test_hits = Hits.objects.create(
+            added_by = testuser1,
+            title = 'This is a test yo',
+            description = 'cannot describe perfection'
+        )
+        test_hits.save()
+
+    def test_hit_content(self):
+        hits = Hits.objects.get(id=1)
+        actual_added_by = str(hits.added_by)
+        actual_title = str(hits.title)
+        actual_description = str(hits.description)
+        self.assertEqual(actual_added_by, 'testuser1')
+        self.assertEqual(actual_title, 'This is a test yo')
+        self.assertEqual(actual_description, 'cannot describe perfection')
